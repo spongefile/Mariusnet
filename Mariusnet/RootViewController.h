e37b1decc86f77c4484d0104b985e8255e9fc968
Tina Aspiala <tinamig@Macintosh-6.local>
2011-10-01 08:51:28 +0300

    Initial Commit

---
+++ Mariusnet/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  Mariusnet
+//
+//  Created by Tina Aspiala on 01/10/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
